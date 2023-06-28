4d58ecb0cbf5e08d91d679a7c49d022128980f95
Wisp <therearewispseverywhere@gmail.com>
2023-06-28 15:10:30 +0300

    add HTML layout for project 24 (Key game)

--- 024-key-game/js/index.js
+++
@@ -1,5 +0,0 @@
-const canvas = document.getElementsByClassName('canvas-one')[0];
-const ctx = canvas.getContext('2d');
-
-/*-------------------------------------------------------------------------------------------------------------*/
-
