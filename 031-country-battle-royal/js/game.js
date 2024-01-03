66bfe5ffb387a3af2cfc7377dc66ea287e0f3d2f
Wisp <therearewispseverywhere@gmail.com>
2024-01-03 21:38:27 +0300

    Project 31 Country battle royal add picking random square in the grid

--- 031-country-battle-royal/js/game.js
+++ 031-country-battle-royal/js/game.js
@@ -1,28 +1,42 @@
-// const game = {
-//     firstCountrySquares: [],
-//     randomSqureXBasedOnRandomPoint: 0,
-//     randomSqureYBasedOnRandomPoint: 0,
+const game = {
+    firstCountrySquares: [],
+    randomSqureXBasedOnRandomPoint: 0,
+    randomSqureYBasedOnRandomPoint: 0,
 
-//     drawGrid: function (w, h) {
-//         let randomPointX = helper.randomIntFromInterval(1, canvas.width);
-//         let randomPointY = helper.randomIntFromInterval(1, canvas.height);
+    worldBioms: ['Desert', 'Forest', 'Mountains', 'Tundra'],
+    worldEvent: ['War', 'Alliance', 'Civil war', 'Unification'],
 
-//         for (let i = 0; i < canvas.width; i += w) {
-//             for (let j = 0; j < canvas.height; j += h) {
-//                 ctx.strokeStyle = '#1b1b1b';
-//                 ctx.strokeRect(i, j, w, h);
-//                 this.randomSqureXBasedOnRandomPoint = randomPointX;
-//                 this.randomSqureYBasedOnRandomPoint = randomPointY;
-//             };
-//         };
+    drawGrid: function (w, h) {
+        let randomPointX = helper.randomIntFromInterval(1, canvas.width);
+        let randomPointY = helper.randomIntFromInterval(1, canvas.height);
 
-//         this.firstCountrySquares = [this.randomSqureXBasedOnRandomPoint, this.randomSqureYBasedOnRandomPoint];
-//     },
-// }
+        for (let i = 0; i < canvas.width; i += w) {
+            for (let j = 0; j < canvas.height; j += h) {
+                ctx.strokeStyle = '#1b1b1b';
+                ctx.strokeRect(i, j, w, h);
+                this.randomSqureXBasedOnRandomPoint = randomPointX;
+                this.randomSqureYBasedOnRandomPoint = randomPointY;
+                // ctx.strokeRect(randomPointX, randomPointY, w, h);
 
-// game.drawGrid(40, 40);
+                if (helper.checkIntersectionBetweenNotRotatedRectangleAndPoint(i + w, i, j + h, j, randomPointX, randomPointY)) {
+                    let randomTopLeftSquarePointX = i;
+                    let randomTopLeftSquarePointY = j;
+                    console.log(randomTopLeftSquarePointX);
+                    console.log(randomTopLeftSquarePointY);
+                    ctx.fillStyle = "green";
+                    ctx.fillRect(randomTopLeftSquarePointX, randomTopLeftSquarePointY, 22, 22);
+                    ctx.fillRect(randomTopLeftSquarePointX, randomTopLeftSquarePointY, 22, 22);
+                };
+            };
+        };
 
-let worldBioms = ['Desert', 'Forest', 'Mountains', 'Tundra'];
+        this.firstCountrySquares = [this.randomSqureXBasedOnRandomPoint, this.randomSqureYBasedOnRandomPoint];
+    },
+};
+
+game.drawGrid(22, 22);
+
+//-------------------------------------------------------------------------------------------------------------------//
 
 function Country(
     points, color, population
@@ -44,17 +58,15 @@ let countries = [
     new Country(100, helper.getRandomColor(), helper.randomIntFromInterval(100000, 500000000)),
 ];
 
-function createCountries() {
-    console.log(countries[1].color);
+// function createCountries() {
+//     for (let i = 0; i < countries.length; i++) {
+//         ctx.fillStyle = countries[i].color;
+//         ctx.fillRect(20 + 450 * i, 20, 100, 100);
+//         ctx.font = "40px serif";
+//         ctx.fillText('Points: ' + countries[i].points, 20 + 450 * i, 170);
+//         ctx.fillText('Population: ' + countries[i].population, 20 + 450 * i, 220);
+//         ctx.fillText('Army: ' + countries[i].army, 20 + 450 * i, 270);
+//     };
+// };
 
-    for (let i = 0; i < countries.length; i++) {
-        ctx.fillStyle = countries[i].color;
-        ctx.fillRect(20 + 450 * i, 20, 100, 100);
-        ctx.font = "40px serif";
-        ctx.fillText('Points: ' + countries[i].points, 20 + 450 * i, 170);
-        ctx.fillText('Population: ' + countries[i].population, 20 + 450 * i, 220);
-        ctx.fillText('Army: ' + countries[i].army, 20 + 450 * i, 270);
-    };
-};
-
-createCountries();
+// createCountries();
