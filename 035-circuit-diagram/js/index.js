61bb535f076ef2a4210bd7f2bf2bd386e07d3ea3
Wisp <therearewispseverywhere@gmail.com>
2024-01-26 20:47:39 +0300

    Project 35 circuit diagram add button and lamp types of tile

--- 035-circuit-diagram/js/index.js
+++ 035-circuit-diagram/js/index.js
@@ -8,8 +8,10 @@ function drawTerminal() {
 };
 
 function drawStartingTile() {
-    let startingTile = new Tile(180, 200, 100, 50, 'wire');
-    startingTile.draw();
+    for (let i = 0; i < 2; i++) {
+        let startingTile = new Tile(180, 200, 100, 50, 'wire');
+        startingTile.draw();
+    };
 };
 
 drawTerminal();
@@ -30,8 +32,42 @@ function Tile(x, y, width, height, type) {
             ctx.beginPath();
             ctx.moveTo(this.x, this.y + this.height / 2);
             ctx.lineTo(this.x + this.width, this.y + this.height / 2);
-            ctx.stroke()
+            ctx.stroke();
+            ctx.closePath();
+        };
+
+        if (this.type === 'button') {
+            ctx.lineWidth = 4;
+            // ctx.strokeRect(180, 200, 100, 50);
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+            ctx.beginPath();
+            ctx.fillStyle = 'rgb(222, 101, 11)'
+            ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.height / 2, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+            ctx.closePath();
+        };
+
+        if (this.type === 'lamp') {
+            ctx.lineWidth = 4;
+            // ctx.strokeRect(180, 200, 100, 50);
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+            ctx.beginPath();
+            ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.height / 2, 0, 2 * Math.PI);
+
+            ctx.moveTo(this.x + this.width / 2, this.y);
+            ctx.lineTo(this.x + this.width / 2, this.y + this.height);
+
+            ctx.moveTo(this.x + 27, this.y + this.height / 2);
+            ctx.lineTo(this.x + this.width - 27, this.y + this.height / 2);
+
+            ctx.stroke();
             ctx.closePath();
         };
     };
+};
+
+for (let i = 0; i < 2; i++) {
+    let newTile = new Tile(380, 200, 100, 50, 'lamp');
+    newTile.draw();
 };
