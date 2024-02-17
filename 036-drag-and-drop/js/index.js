8ef75c43fae896b60924adba25b19776d5a192ea
Wisp <therearewispseverywhere@gmail.com>
2024-02-17 21:42:06 +0300

    Project 36 drag and drop add helper and dragging and dropping

--- 036-drag-and-drop/js/index.js
+++ 036-drag-and-drop/js/index.js
@@ -3,15 +3,32 @@ const ctx = canvas.getContext('2d');
 
 /*--------------------------------------------------------------------------------*/
 
-function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
+let helper = {
+    checkIntersectionBetweenNotRotatedRectangleAndPoint: function (
+        farX, closeX,
+        farY, closeY,
+        pointX, pointY,
+    ) {
+        if (closeX <= pointX &&
+            farX >= pointX &&
+            closeY <= pointY &&
+            farY >= pointY) {
+            return true;
+        } else {
+            return false;
+        };
+    },
 
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    };
+    getRandomColor: function () {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
 
-    return color;
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        };
+
+        return color;
+    }
 };
 
 function Square(x, y, width, height) {
@@ -19,32 +36,49 @@ function Square(x, y, width, height) {
     this.y = y;
     this.width = width;
     this.height = height;
-    this.color = getRandomColor();
+    this.color = helper.getRandomColor();
 
     this.draw = function () {
+        setTimeout(() => {
+            this.draw();
+        }, 1);
+
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     };
 };
 
-let squareOne = new Square(400, 300, 120, 120);
-squareOne.draw();
-
 let mousePose = {};
 
 let previousTickMouseX = 0;
 let currentTickMouseX = 0;
-let currentMouseDirectionX = 'nowhere';
+let isMousedown = false;
+let startX = 0;
+let startY = 0;
 
 window.addEventListener('mousemove', (e) => {
     let bounding = canvas.getBoundingClientRect();
     mousePose.currentMouseX = e.clientX - bounding.left;
     mousePose.currentMouseY = e.clientY - bounding.top;
+
+    if (isMousedown === true) {
+        moveSquare();
+    };
+});
+
+window.addEventListener('mousedown', (e) => {
+    startX = mousePose.currentMouseX;
+    startY = mousePose.currentMouseY;
+    isMousedown = true;
+});
+
+window.addEventListener('mouseup', (e) => {
+    isMousedown = false;
 });
 
 function showMousePosition() {
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     setTimeout(() => {
         showMousePosition();
@@ -56,4 +90,27 @@ function showMousePosition() {
     ctx.fillText(`currentMouseY: ${mousePose.currentMouseY}`, 50, 190);
 };
 
+function moveSquare() {
+    if (helper.checkIntersectionBetweenNotRotatedRectangleAndPoint(
+        squareOne.x + squareOne.width, squareOne.x,
+        squareOne.y + squareOne.height, squareOne.y,
+        mousePose.currentMouseX, mousePose.currentMouseY)
+    ) {
+        // let xDifference = squareOne.x + squareOne.width - mousePose.currentMouseX;
+        // let yDifference = squareOne.y + squareOne.height - mousePose.currentMouseY;
+        let xDifference = mousePose.currentMouseX - startX;
+        let yDifference = mousePose.currentMouseY - startY;
+        squareOne.x += xDifference;
+        squareOne.y += yDifference;
+
+        startX++;
+        startY++;
+    } else {
+        console.log('wjehfg');
+    };
+};
+
 showMousePosition();
+
+let squareOne = new Square(400, 300, 120, 120);
+squareOne.draw();
