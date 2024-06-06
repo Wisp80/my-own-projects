d6f8bf68b6fdfac7db326267fac516c7ad7f9d7d
Wisp <therearewispseverywhere@gmail.com>
2024-06-06 18:53:14 +0300

    Project 44 fire animation add project and make a template of a particle

---
+++ 044-fire-animation/js/index.js
@@ -0,0 +1,53 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+/*-------------------------------------------------------------------------------------------------------------*/
+
+function getRandomColor() {
+    let letters = '012345678ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    };
+
+    return color;
+};
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+};
+
+function fire() {
+    // setTimeout(() => {
+    //     fire();
+    // }, 100);
+
+    let bigCircleRadius = 70;
+    let bigCircleColor = getRandomColor();
+
+    let smallCircleRadius = 68;
+    let smallCircleColor = 'white'
+
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, bigCircleRadius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = bigCircleColor;
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2 + 40, canvas.height / 2, smallCircleRadius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = smallCircleColor;
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2 + 2 * bigCircleRadius - 8, bigCircleRadius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = bigCircleColor;
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2 - 40, canvas.height / 2 + 2 * bigCircleRadius - 8, smallCircleRadius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = smallCircleColor;
+    ctx.fill();
+};
+
+fire();
