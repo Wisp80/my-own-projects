1dc00acbf06315b2942d30ac24170b8cd576ba7e
Wisp <therearewispseverywhere@gmail.com>
2024-06-08 00:12:23 +0300

    Project 44 fire animation make particle rotate around the center of the cnavas

--- 044-fire-animation/js/index.js
+++ 044-fire-animation/js/index.js
@@ -18,8 +18,7 @@ function getRandomNumber(max) {
     return Math.floor(Math.random() * max);
 };
 
-let radius = 200;
-let radiusTwo = 400;
+let radius = 100;
 let i = 0;
 let steps = 10;
 let xOne;
@@ -34,35 +33,38 @@ let smallCircleRadius = 68;
 let smallCircleColor = 'white'
 
 function fire() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     xOne = (canvas.width / 2 + radius * Math.cos(2 * Math.PI * i / steps));
     yOne = (canvas.height / 2 + radius * Math.sin(2 * Math.PI * i / steps));
 
     xTwo = (canvas.width / 2 - radius * Math.cos(2 * Math.PI * i / steps));
     yTwo = (canvas.height / 2 - radius * Math.sin(2 * Math.PI * i / steps));
 
-    i = i + 0.05;
+    i = i + 0.25;
 
     ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, bigCircleRadius, 0, 2 * Math.PI, false);
+    ctx.arc(xOne, yOne, bigCircleRadius, 0, 2 * Math.PI, false);
     ctx.fillStyle = bigCircleColor;
     ctx.fill();
 
     ctx.beginPath();
-    ctx.arc(canvas.width / 2 + 40, canvas.height / 2, smallCircleRadius, 0, 2 * Math.PI, false);
+    ctx.arc(xOne + 40, yOne, smallCircleRadius, 0, 2 * Math.PI, false);
     ctx.fillStyle = smallCircleColor;
     ctx.fill();
 
     ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2 + 2 * bigCircleRadius - 8, bigCircleRadius, 0, 2 * Math.PI, false);
+    ctx.arc(xOne, yOne + 2 * bigCircleRadius - 8, bigCircleRadius, 0, 2 * Math.PI, false);
     ctx.fillStyle = bigCircleColor;
     ctx.fill();
 
     ctx.beginPath();
-    ctx.arc(canvas.width / 2 - 40, canvas.height / 2 + 2 * bigCircleRadius - 8, smallCircleRadius, 0, 2 * Math.PI, false);
+    ctx.arc(xOne - 40, yOne + 2 * bigCircleRadius - 8, smallCircleRadius, 0, 2 * Math.PI, false);
     ctx.fillStyle = smallCircleColor;
     ctx.fill();
 };
 
-setTimeout(() => {
+setInterval(() => {
     fire();
 }, 1);
