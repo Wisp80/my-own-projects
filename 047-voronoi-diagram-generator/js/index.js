da4dd2cde1f93a7a61c8d94e8c723b3abacf3ab7
Wisp <therearewispseverywhere@gmail.com>
2024-07-17 21:27:43 +0300

    Project 47 Voronoi diagram generator add Voronoi diagram generator

--- 047-voronoi-diagram-generator/js/index.js
+++ 047-voronoi-diagram-generator/js/index.js
@@ -4,3 +4,69 @@ const ctx = canvas.getContext('2d');
 window.onload = function () {
     // controls.initializePlayersControlsListening();
 };
+
+let mainPoints = [
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+    [helper.randomIntFromInterval(0, 795), helper.randomIntFromInterval(0, 795), helper.getRandomColor()],
+];
+
+function getDistanceBetweenTwoPixels(x1, y1, x2, y2) {
+    let distance = 0;
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+
+    dx = x2 - x1;
+    dy = y2 - y1;
+    distance = Math.sqrt(dx * dx + dy * dy);
+
+    return distance;
+};
+
+function draw() {
+    let distanceBetweenThisPixelAndPointOne = 0;
+    let distanceBetweenThisPixelAndPointTwo = 0;
+
+    for (let i = 0; i < canvas.width; i++) {
+        for (let j = 0; j < canvas.height; j++) {
+            let minDistance = Infinity;
+            let closestPointIndex = 0;
+
+            for (let k = 0; k < mainPoints.length; k++) {
+                let distance = getDistanceBetweenTwoPixels(i, j, mainPoints[k][0], mainPoints[k][1]);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestPointIndex = k;
+                };
+            };
+
+            ctx.fillStyle = mainPoints[closestPointIndex][2];
+            ctx.fillRect(i, j, 1, 1);
+
+            distanceBetweenThisPixelAndPointOne = getDistanceBetweenTwoPixels(i, j, mainPoints[0][0], mainPoints[0][1]);
+            distanceBetweenThisPixelAndPointTwo = getDistanceBetweenTwoPixels(i, j, mainPoints[1][0], mainPoints[1][1]);
+        };
+
+        for (let i = 0; i < mainPoints.length; i++) {
+            ctx.fillStyle = 'black';
+            ctx.fillRect(mainPoints[i][0], mainPoints[i][1], 5, 5);
+        }
+    };
+};
+
+draw();
+
+//200
